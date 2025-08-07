51db3dcdbe958e3cd400e2f294eee2a4292b05e5
Monara <Jaidenhema@gmail.com>
2025-08-07 02:00:21 +0100

    Update and rename Roblox Friend Cleaner-1.0.user.js to roblox-friend-cleaner-1.0.1.user.js

--- roblox-friend-cleaner-1.0.1.user.js
+++ roblox-friend-cleaner-1.0.1.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Roblox Friend Cleaner
 // @namespace    https://monarahema.github.io/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Easily unfriend old Roblox friends in bulk
 // @author       MonaraHema
 // @match        https://www.roblox.com/*friends*
